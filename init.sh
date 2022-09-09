cb23ca0099d6e1f377a6a5308c3a828b72fcb5f5
izlyforever <72483314+izlyforever@users.noreply.github.com>
2022-09-09 10:27:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,28 +3,12 @@
 rm -rf .git
 git init .
 
-git clone git@github.com:izlyforever/AgoraLite.git
 git clone git@github.com:izlyforever/Blog.git
 git clone git@github.com:izlyforever/cf.git
-git clone git@github.com:izlyforever/ICS2021PA.git
-git clone git@github.com:izlyforever/learnASM.git
-git clone git@github.com:izlyforever/learnLinuxC.git
-git clone git@github.com:izlyforever/learnOS.git
-git clone git@github.com:izlyforever/Problems.git
-
-git clone git@github.com:izlyforever/cf-tool.git release/cf-tool
-git clone git@github.com:izlyforever/ChangeMD.git release/ChangeMD
-git clone git@github.com:izlyforever/mkdocs.git release/mkdocs
-git clone git@github.com:izlyforever/traceAnalysis.git release/traceAnalysis
-
+git clone git@github.com:izlyforever/cf-tool.git
 git clone git@github.com:izlyforever/cpplibforCP.git cf/cpplibforCP
 
 cd cf
-
-../cit.sh config --local user.name izlyforever
-../cit.sh config --local user.email izlyforever@outlook.com
-
-cd ../release
 
 ../cit.sh config --local user.name izlyforever
 ../cit.sh config --local user.email izlyforever@outlook.com
